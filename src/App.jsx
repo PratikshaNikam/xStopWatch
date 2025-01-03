5aa47a0d8245212f0d5651607235cdd89b4bcbee
Pratiksha Nikam <pratiksha.gawande@gmail.com>
2025-01-03 14:31:58 +0530

    stopwatch code is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState,useEffect } from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isRunning, setIsRunning] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState(0);
 
+
+  useEffect(() => {
+    let intervalId;
+    if (isRunning) {
+      intervalId = setInterval(() => {
+        setElapsedTime((prevElapsedTime) => prevElapsedTime + 1);
+      }, 1000);
+    }
+
+    else {
+      clearInterval(intervalId);
+    }
+
+    return () => clearInterval(intervalId);
+  }, [isRunning])
+  
+  const startStop = () => {
+    setIsRunning(isRunning => !isRunning);
+  }
+
+  const reset = () => {
+    setIsRunning(false);
+    setElapsedTime(0);
+  }
+ 
+  const formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+
+    return `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
+  }
   return (
     <>
       <h1>Stopwatch</h1>  
+
+      <h2>Time:{formatTime(elapsedTime)}</h2>
+
+      <button onClick={startStop}>{isRunning?"Stop":"Start"}</button>
+      <button onClick={reset}>Reset</button>
     </>
   )
 }
